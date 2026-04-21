13321865f23ceacaa82534f1e43f504f6536678f
mathiskacer2 <mathis.kacer@eleve.ensai.fr>
2026-04-21 08:49:47 +0000

    verifications plus modifications du tri pour eviter les bugs

--- data/tri_dossier.py
+++ data/tri_dossier.py
@@ -2,15 +2,15 @@ import os
 import re
 import shutil
 
-
 def trier_fichiers(dossier_source, dossier_mains, dossier_resumes):
     """
     Trie les fichiers de mains de poker en fonction de leur type (mains ou résumés)
     et les déplace dans les dossiers appropriés.
     """
+    os.makedirs(dossier_mains, exist_ok=True)
+    os.makedirs(dossier_resumes, exist_ok=True)
 
     for nom_fichier in os.listdir(dossier_source):
-
         chemin_initial = os.path.join(dossier_source, nom_fichier)
 
         if not nom_fichier.endswith(".txt"):
@@ -18,9 +18,9 @@ def trier_fichiers(dossier_source, dossier_mains, dossier_resumes):
 
         if "Expresso" in nom_fichier and "_summary" in nom_fichier:
             chemin_destination = os.path.join(dossier_resumes, nom_fichier)
-
         elif "Expresso" in nom_fichier:
             chemin_destination = os.path.join(dossier_mains, nom_fichier)
+        else:
+            continue
 
-        # 3. On déplace le fichier
         shutil.move(chemin_initial, chemin_destination)
