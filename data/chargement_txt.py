3da28a5ac29e98430e5aa48364db8eb33626ae59
mathiskacer2 <mathis.kacer@eleve.ensai.fr>
2026-04-21 13:54:44 +0000

    solution pour que bdd soit dispo pour tous

--- data/chargement_txt.py
+++ data/chargement_txt.py
@@ -3,14 +3,19 @@ import zipfile
 import os
 
 
+import urllib.request
+import zipfile
+import os
+
 def load_data_from_s3(project_dir, s3_path, endpoint_url="https://minio.lab.sspcloud.fr"):
     zip_path = os.path.join(project_dir, "data/history.zip")
     extract_path = os.path.join(project_dir, "data/historique/")
     
+    os.makedirs(os.path.dirname(zip_path), exist_ok=True)
     os.makedirs(extract_path, exist_ok=True)
     
-    fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": endpoint_url})
-    fs.get(s3_path, zip_path)
+    url = f"{endpoint_url}/{s3_path}"
+    urllib.request.urlretrieve(url, zip_path)
     
     with zipfile.ZipFile(zip_path, "r") as z:
         z.extractall(extract_path)
